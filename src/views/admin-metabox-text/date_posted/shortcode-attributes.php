64f660bd3d932bcced94401e158deb79bc721652
Jeff Cleverley <jeff@jeffcleverley.com>
2018-05-20 16:19:59 +0800

    added class and style atts to date_posted

--- src/views/admin-metabox-text/date_posted/shortcode-attributes.php
+++ src/views/admin-metabox-text/date_posted/shortcode-attributes.php
@@ -14,6 +14,16 @@
 		<td>Displayed after the date posted</td>
 		<td>Empty string</td>
 	</tr>
+    <tr>
+        <td>class</td>
+        <td>Additional classes to add to the span element</td>
+        <td>Empty string</td>
+    </tr>
+    <tr>
+        <td>style</td>
+        <td>Inline CSS styling to add to the span element</td>
+        <td>Empty string</td>
+    </tr>
 	<tr>
 		<td>date-format</td>
 		<td>The formatting for the date.</td>
