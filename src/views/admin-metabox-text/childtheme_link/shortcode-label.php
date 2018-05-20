123b6e4df51191c60f85a107bf7ca1b381ee16e4
Jeff Cleverley <jeff@jeffcleverley.com>
2018-05-20 16:34:44 +0800

    added span/link style and class atts to childtheme_link

--- src/views/admin-metabox-text/childtheme_link/shortcode-label.php
+++ src/views/admin-metabox-text/childtheme_link/shortcode-label.php
@@ -1,3 +1,3 @@
-Shortcode that adds a link to the child theme.
-Supported attributes are <strong>before</strong>, <strong>after</strong>, <strong>child-theme-name</strong>, and <strong>child-theme-url</strong>:
+Shortcode that adds a link to the child theme. Link is displayed inside a &lt;span&gt; element.
+Supported attributes are <strong>before</strong>, <strong>after</strong>, <strong>span-class</strong>, <strong>span-style</strong>, <strong>link-class</strong>, <strong>link-style</strong>, and <strong>child-theme-url</strong>:
 
