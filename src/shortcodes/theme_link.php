de466630ab392c537f684f94ce9748d6a020f6c9
Jeff Cleverley <jeff@jeffcleverley.com>
2018-05-20 18:26:14 +0800

    added class and style attributes to span/link elements of theme_link

--- src/shortcodes/theme_link.php
+++ src/shortcodes/theme_link.php
@@ -9,10 +9,14 @@ namespace LearningCurve\BeansSimpleShortcodes;
  * Shortcode that adds a link to the Beans Theme Framework within a span element.
  *
  * Supported shortcode attributes are:
- *          before      Output after link but inside span, default is empty string.
- *          after       Output after link but inside span, default is empty string.
- *          beans       Name of Beans theme, default is 'Beans'.
- *          beans-url   Url of the Beans theme framework, default is https://getbeans.io/.
+ *          before          Output after link but inside span, default is empty string.
+ *          after           Output after link but inside span, default is empty string.
+ *          beans           Name of Beans theme, default is 'Beans'.
+ *          beans-url       Url of the Beans theme framework, default is https://getbeans.io/.
+ *          span-class      Additional classes to add to the span element, default is empty string.
+ *          span-style      Inline CSS to style the span element, default is empty string.
+ *          link-class      Additional classes to add to the link anchor element, default is empty string.
+ *          link-style      Inline CSS to style the link anchor element, default is empty string.
  *
  * Defaults pass through `beans_link_shortcode_defaults` filter.
  * Output passes through `beans_link_shortcode` filter before returning.
@@ -30,6 +34,10 @@ function theme_link_shortcode( $atts ) {
 		'after'     => '',
 		'beans'     => __( 'Beans', BEANS_SIMPLE_SHORTCODES ),
 		'beans-url' => 'https://getbeans.io',
+		'span-class' => '',
+		'span-style' => '',
+		'link-class' => '',
+		'link-style' => '',
 	);
 
 	/**
@@ -49,13 +57,16 @@ function theme_link_shortcode( $atts ) {
 	ob_start();
 
 	beans_open_markup_e( 'beans_simple_beans_link_span', 'span', array(
-		'style' => 'color:inherit;',
+		'class' => $atts['span-class'],
+		'style' => 'color:inherit; ' . $atts['span-style'],
 	) );
 
 	beans_output_e( 'beans_simple_beans_link_text_prefix', $atts['before'] );
 
 	beans_open_markup_e( 'beans_simple_beans_link', 'a', array(
 		'href' => $atts['beans-url'],
+		'class' => $atts['link-class'],
+		'style' => $atts['link-style'],
 	) );
 
 	beans_output_e( 'beans_simple_beans_link_text', $atts['beans'] );
--- src/views/admin-metabox-text/post_author_link/shortcode-label.php
+++ src/views/admin-metabox-text/post_author_link/shortcode-label.php
@@ -1,3 +1,3 @@
 Shortcode that displays the post author's name as a link to their archive url. Link and attributes are displayed inside a &lt;span&gt; element.</br>
-Supported attributes are <strong>before</strong>, <strong>after</strong>,  <strong>span-class</strong>, <strong>span-style</strong>, <strong>link-class</strong>, and <strong>link-style</strong>:
+Supported attributes are <strong>before</strong>, <strong>after</strong>, <strong>span-class</strong>, <strong>span-style</strong>, <strong>link-class</strong>, and <strong>link-style</strong>:
 
